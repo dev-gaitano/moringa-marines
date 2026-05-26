c0d8ace78e3f7d24711dfd3c6c8becfed7bb4ffd
gaitano <dev.gaitano@gmail.com>
2026-05-26 09:05:43 +0300

    feat(module-4): add data structures

--- module-4/day_1.py
+++ module-4/day_1.py
@@ -11,6 +11,25 @@ my_num: int = 1234
 print(type(my_num))
 print(my_num)
 
+# Lists
+fruits = ["apple", "banana", "orange"]
+print(fruits[0])  # Output: "apple"
+fruits.append("grape")
+print(fruits)  # Output: ["apple", "banana", "orange", "grape"]
+
+# tuples
+point = (3, 4)
+print(point[0])  # Output: 3
+x, y = point  # Tuple unpacking
+print(x)  # Output: 3
+print(y)  # Output: 4
+
+# dicts
+person = {"name": "John", "age": 25, "city": "New York"}
+print(person["name"])  # Output: "John"
+person["age"] = 30
+print(person)  # Output: {"name": "John", "age": 30, "city": "New York"}
+
 # Operators and Control flow statements
 age: int = 190
 user_registered: bool = True
