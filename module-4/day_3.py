27d6ae44a5fde4b4a0892bc7e69354fd5118b340
gaitano <dev.gaitano@gmail.com>
2026-05-28 15:57:56 +0300

    feat(module-4): add data structures lesson file

---
+++ module-4/day_3.py
@@ -0,0 +1,21 @@
+# =======================================
+# Data structures
+# =======================================
+
+# Lists
+# - Ordered
+# - Mutable
+# - Allows duplicates
+fruits: list = ["orange", "mangoes", "nanasi"]
+
+# Tuples 
+# - Ordered
+# - Immutable (Fater than a list)
+# - Allows duplicates
+cords: tuple = (56, 32)
+
+# Sets
+# - Unordered
+# - Immutable
+# - Doesn't allow duplicates
+nums = set(1, 2, 3, 4, 5)
